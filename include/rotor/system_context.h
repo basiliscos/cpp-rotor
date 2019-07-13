194dda6d3fcfc3429d26d4627608ceb59bba0f00
Ivan Baidakou <the.dmol@yandex.by>
2019-07-13 21:27:46 +0300

    Update docs

--- include/rotor/system_context.h
+++ include/rotor/system_context.h
@@ -15,10 +15,19 @@ namespace rotor {
 struct supervisor_t;
 using supervisor_ptr_t = intrusive_ptr_t<supervisor_t>;
 
+/** \struct system_context_t
+ *  \brief The system context holds root {@link supervisor_t}
+ * (intrusive pointer) and may be loop-related details in derived classes
+ *
+ */
 struct system_context_t : arc_base_t<system_context_t> {
   public:
+
+    /** \brief creates root supervior. `args` are forwared for supervisor constructor */
     template <typename Supervisor = supervisor_t, typename... Args>
     auto create_supervisor(Args... args) -> intrusive_ptr_t<Supervisor>;
+
+    /** \brief returns root supervisor */
     inline supervisor_ptr_t get_supervisor() noexcept { return supervisor; }
 
     system_context_t();
@@ -26,6 +35,14 @@ struct system_context_t : arc_base_t<system_context_t> {
     system_context_t(const system_context_t &) = delete;
     system_context_t(system_context_t &&) = delete;
     virtual ~system_context_t();
+
+    /** \brief fatal error handler
+     *
+     * The error is fatal, is further `rotor` behaviour is undefined. The method should
+     * be overriden in derived classes for error propagation/notification. The default
+     * implementation is to output the error to `std::err` and invoke `std::abort()`.
+     *
+     */
     virtual void on_error(const std::error_code &ec) noexcept;
 
   private:
