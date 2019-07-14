7921e780189d5e315efa318e659196f8c7f810c9
Ivan Baidakou <the.dmol@yandex.by>
2019-07-14 15:12:54 +0300

    more docs

--- include/rotor/handler.hpp
+++ include/rotor/handler.hpp
@@ -20,30 +20,65 @@ struct actor_base_t;
 struct supervisor_t;
 using supervisor_ptr_t = intrusive_ptr_t<supervisor_t>;
 
+/** \struct handler_traits
+ *  \brief Helper class to extract final actor class and message type from pointer-to-member function
+ */
 template <typename T> struct handler_traits {};
+
+/** \struct handler_traits<void (A::*)(M &) noexcept>
+ *  \brief Helper class to extract final actor class and message type from pointer-to-member function
+ */
 template <typename A, typename M> struct handler_traits<void (A::*)(M &) noexcept> {
+    /** \brief final class of actor */
     using actor_t = A;
+
+    /** \brief message type, processed by the handler */
     using message_t = M;
 };
 
+/** \struct handler_base_t
+ *  \brief Base class for `rotor` handler, i.e concrete message type processing point
+ * on concrete actor
+ */
 struct handler_base_t : public arc_base_t<handler_base_t> {
-    // actor_base_t *raw_actor_ptr; /* non-owning pointer to actor address */
-    const void *raw_actor_ptr; /* non-owning pointer to actor address */
+    /** \brief pointer to unique message type ( `typeid(Message).name()` ) */
     const void *message_type;
+
+    /** \brief pointer to unique handler type ( `typeid(Handler).name()` ) */
     const void *handler_type;
+
+    /** \brief intrusive poiter to {@link supervisor_t} which owns the
+     * actor of the handler
+     */
     supervisor_ptr_t supervisor;
+
+    /** \brief intrusive poiter to {@link address_t} of the actor of the handler */
     address_ptr_t actor_addr;
+
+    /** \brief precalculated hash for the handler */
     size_t precalc_hash;
+
+    /** \brief constructs `handler_base_t` from raw pointer to actor, raw
+     * pointer to message type and raw pointer to handler type
+     */
     handler_base_t(actor_base_t *actor, const void *message_type_, const void *handler_type_)
-        : raw_actor_ptr{actor}, message_type{message_type_}, handler_type{handler_type_},
+        : message_type{message_type_}, handler_type{handler_type_},
           supervisor(&actor->get_supervisor()), actor_addr{actor->get_address()} {
-        auto h1 = reinterpret_cast<std::size_t>(raw_actor_ptr);
-        auto h2 = reinterpret_cast<std::size_t>(handler_type);
+        auto h1 = reinterpret_cast<std::size_t>(handler_type);
+        auto h2 = reinterpret_cast<std::size_t>(actor_addr.get());
         precalc_hash = h1 ^ (h2 << 1);
     }
+
+    /** \brief compare two handler for equality */
     inline bool operator==(const handler_base_t &rhs) const noexcept {
-        return handler_type == rhs.handler_type && raw_actor_ptr == rhs.raw_actor_ptr;
+        return handler_type == rhs.handler_type && actor_addr == rhs.actor_addr;
     }
+
+    /** \brief attempt to delivery message to he handler
+     *
+     * The message is delivered only if its type matches to the handler message type,
+     * otherwise it is silently ignored
+     */
     virtual void call(message_ptr_t &) noexcept = 0;
 
     virtual inline ~handler_base_t() {}
@@ -52,10 +87,6 @@ struct handler_base_t : public arc_base_t<handler_base_t> {
 using handler_ptr_t = intrusive_ptr_t<handler_base_t>;
 
 template <typename Handler> struct handler_t : public handler_base_t {
-    using traits = handler_traits<Handler>;
-    using final_message_t = typename traits::message_t;
-    using final_actor_t = typename traits::actor_t;
-
     static const void *handler_type;
     Handler handler;
     actor_ptr_t actor_ptr;
@@ -72,6 +103,10 @@ template <typename Handler> struct handler_t : public handler_base_t {
             (final_obj.*handler)(*final_message);
         }
     }
+private:
+    using traits = handler_traits<Handler>;
+    using final_message_t = typename traits::message_t;
+    using final_actor_t = typename traits::actor_t;
 };
 
 template <typename Handler>
