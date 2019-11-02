cdd8ff0ce03a6e08882107913a0460d082b43c3b
Ivan Baidakou <the.dmol@yandex.by>
2019-11-02 16:03:34 +0300

    more tests

--- tests/023-supervisor-children.cpp
+++ tests/023-supervisor-children.cpp
@@ -13,7 +13,6 @@ namespace r = rotor;
 namespace rt = r::test;
 
 struct sample_actor_t : public rt::actor_test_t {
-
     using rt::actor_test_t::actor_test_t;
 
     virtual void init_start() noexcept override {}
@@ -22,6 +21,18 @@ struct sample_actor_t : public rt::actor_test_t {
         r::actor_base_t::init_start();
     }
 
+};
+
+struct sample_supervisor_t: public rt::supervisor_test_t {
+    using rt::supervisor_test_t::supervisor_test_t;
+    using child_ptr_t = r::intrusive_ptr_t<sample_actor_t>;
+
+    virtual void init_start() noexcept override {
+        auto timeout = r::pt::milliseconds{1};
+        child= create_actor<sample_actor_t>(timeout);
+    }
+
+    child_ptr_t child;
 };
 
 TEST_CASE("supervisor is not initialized, while it child did not confirmed initialization", "[supervisor]") {
@@ -102,4 +113,27 @@ TEST_CASE("supervisor does not starts, if a children did not initialized", "[sup
     REQUIRE(sup->get_state() == r::state_t::SHUTTED_DOWN);
     REQUIRE(act1->get_state() == r::state_t::SHUTTED_DOWN);
     REQUIRE(act2->get_state() == r::state_t::SHUTTED_DOWN);
+}
+
+
+TEST_CASE("supervisor create child during initi phase", "[supervisor]") {
+    r::system_context_t system_context;
+    const void *locality = &system_context;
+
+    auto timeout = r::pt::milliseconds{1};
+    rt::supervisor_config_test_t config(timeout, locality);
+    auto sup = system_context.create_supervisor<sample_supervisor_t>(nullptr, config);
+    auto& act = sup->child;
+
+    sup->do_process();
+    REQUIRE(sup->get_state() == r::state_t::INITIALIZING);
+    REQUIRE(act->get_state() == r::state_t::INITIALIZING);
+
+    act->confirm_init_start();
+    sup->do_process();
+    REQUIRE(sup->get_state() == r::state_t::OPERATIONAL);
+    REQUIRE(act->get_state() == r::state_t::OPERATIONAL);
+
+    sup->do_shutdown();
+    sup->do_process();
 }
