3dbba75e6f126810cf5b379e7854f8ffb596ca3e
Ivan Baidakou <the.dmol@yandex.by>
2020-04-06 09:20:42 +0300

    Remove copy-pastes

--- include/rotor/asio/forwarder.hpp
+++ include/rotor/asio/forwarder.hpp
@@ -46,9 +46,41 @@ template <typename Class> struct callback_traits<void (Class::*)() noexcept> {
 /** \brief return `strand` of the boost::asio aware actor */
 template <typename Actor> inline boost::asio::io_context::strand &get_strand(Actor &actor);
 
+template <typename Actor, typename Handler, typename ErrHandler> struct forwarder_base_t;
+
+template <typename Actor, typename Handler> struct forwarder_base_t<Actor, Handler, void> {
+    /** \brief alias for intrusive pointer for an actor type */
+    using typed_actor_ptr_t = intrusive_ptr_t<Actor>;
+
+    /** \brief alias for boost::asio supervior */
+    using typed_sup_t = supervisor_asio_t;
+
+    /** \brief constructs forwarder_base for the actor from it handler function */
+    forwarder_base_t(Actor &actor_, Handler &&handler_) : typed_actor{&actor_}, handler{std::move(handler_)} {}
+
+    /** intrusive pointer to the actor */
+    typed_actor_ptr_t typed_actor;
+
+    /** pointer-to-member function of the actor to handle positive case result */
+    Handler handler;
+};
+
+template <typename Actor, typename Handler, typename ErrHandler>
+struct forwarder_base_t : forwarder_base_t<Actor, Handler, void> {
+    using base_t = forwarder_base_t<Actor, Handler, void>;
+
+    /** \brief constructs forwarder_base for the actor from it's member functions: positive
+     * result handler and error handler functions.
+     */
+    forwarder_base_t(Actor &actor_, Handler &&handler_, ErrHandler &&err_handler_)
+        : base_t{actor_, std::move(handler_)}, err_handler{std::move(err_handler_)} {}
+
+    /** pointer-to-member function of the actor to handle negative case result, i.e. `error_code` */
+    ErrHandler err_handler;
+};
+
 template <typename Actor, typename Handler, typename ArgsCount, typename ErrHandler> struct forwarder_t {};
 
-// template <typename Actor, typename Handler, typename ErrHandler = void> struct forwarder_t;
 /**
  * \brief dispatches single boost::asio callback with `error_code` and non-error results
  * into two differnt functions of the `actor`. After the invocation, actor's supervisor
@@ -58,148 +90,97 @@ template <typename Actor, typename Handler, typename ArgsCount, typename ErrHand
  *
  */
 template <typename Actor, typename Handler, typename ErrHandler>
-struct forwarder_t<Actor, Handler, details::count::_0, ErrHandler> {
-    /** \brief alias for intrusive pointer for an actor type */
-    using typed_actor_ptr_t = intrusive_ptr_t<Actor>;
-
-    /** \brief alias for boost::asio supervior */
-    using typed_sup_t = supervisor_asio_t;
-
-    /** \brief constructs forwarder for the actor from it's member functions: positive
-     * result handler and error handler functions.
-     */
-    forwarder_t(Actor &actor_, Handler &&handler_, ErrHandler &&err_handler_)
-        : typed_actor{&actor_}, handler{std::move(handler_)}, err_handler{std::move(err_handler_)} {}
+struct forwarder_t<Actor, Handler, details::count::_0, ErrHandler> : forwarder_base_t<Actor, Handler, ErrHandler> {
+    using base_t = forwarder_base_t<Actor, Handler, ErrHandler>;
+    using base_t::base_t;
 
     /** \brief mimics boost::asio handler, which will be forwarded/decomposed into
      * two different methods of the actor
      */
     template <typename T = void> inline void operator()(const boost::system::error_code &ec) noexcept {
-        auto &sup = static_cast<typed_sup_t &>(typed_actor->get_supervisor());
+        using typed_sup_t = typename base_t::typed_sup_t;
+        auto &sup = static_cast<typed_sup_t &>(base_t::typed_actor->get_supervisor());
         auto &strand = get_strand(sup);
         if (ec) {
-            asio::defer(strand, [actor = typed_actor, handler = std::move(err_handler), ec = ec]() {
+            asio::defer(strand, [actor = base_t::typed_actor, handler = std::move(base_t::err_handler), ec = ec]() {
                 ((*actor).*handler)(ec);
                 actor->get_supervisor().do_process();
             });
         } else {
-            asio::defer(strand, [actor = typed_actor, handler = std::move(handler)]() {
+            asio::defer(strand, [actor = base_t::typed_actor, handler = std::move(base_t::handler)]() {
                 ((*actor).*handler)();
                 actor->get_supervisor().do_process();
             });
         }
     }
-
-    /** intrusive pointer to the actor */
-    typed_actor_ptr_t typed_actor;
-
-    /** pointer-to-member function of the actor to handle positive case result */
-    Handler handler;
-
-    /** pointer-to-member function of the actor to handle negative case result, i.e. `error_code` */
-    ErrHandler err_handler;
 };
 
 template <typename Actor, typename Handler, typename ErrHandler>
-struct forwarder_t<Actor, Handler, details::count::_1, ErrHandler> {
-    /** \brief alias for intrusive pointer for an actor type */
-    using typed_actor_ptr_t = intrusive_ptr_t<Actor>;
-
-    /** \brief alias for boost::asio supervior */
-    using typed_sup_t = supervisor_asio_t;
-
-    /** \brief constructs forwarder for the actor from it's member functions: positive
-     * result handler and error handler functions.
-     */
-    forwarder_t(Actor &actor_, Handler &&handler_, ErrHandler &&err_handler_)
-        : typed_actor{&actor_}, handler{std::move(handler_)}, err_handler{std::move(err_handler_)} {}
+struct forwarder_t<Actor, Handler, details::count::_1, ErrHandler> : forwarder_base_t<Actor, Handler, ErrHandler> {
+    using base_t = forwarder_base_t<Actor, Handler, ErrHandler>;
+    using base_t::base_t;
 
     /** \brief mimics boost::asio handler, which will be forwarded/decomposed into
      * two different methods of the actor
      */
     template <typename T> inline void operator()(const boost::system::error_code &ec, T arg) noexcept {
-        auto &sup = static_cast<typed_sup_t &>(typed_actor->get_supervisor());
+        using typed_sup_t = typename base_t::typed_sup_t;
+        auto &sup = static_cast<typed_sup_t &>(base_t::typed_actor->get_supervisor());
         auto &strand = get_strand(sup);
         if (ec) {
-            asio::defer(strand, [actor = typed_actor, handler = std::move(err_handler), ec = ec]() {
+            asio::defer(strand, [actor = base_t::typed_actor, handler = std::move(base_t::err_handler), ec = ec]() {
                 ((*actor).*handler)(ec);
                 actor->get_supervisor().do_process();
             });
         } else {
-            asio::defer(strand, [actor = typed_actor, handler = std::move(handler), arg = std::move(arg)]() mutable {
+            asio::defer(strand, [actor = base_t::typed_actor, handler = std::move(base_t::handler),
+                                 arg = std::move(arg)]() mutable {
                 ((*actor).*handler)(std::move(arg));
                 actor->get_supervisor().do_process();
             });
         }
     }
-
-    /** intrusive pointer to the actor */
-    typed_actor_ptr_t typed_actor;
-
-    /** pointer-to-member function of the actor to handle positive case result */
-    Handler handler;
-
-    /** pointer-to-member function of the actor to handle negative case result, i.e. `error_code` */
-    ErrHandler err_handler;
 };
 
-template <typename Actor, typename Handler> struct forwarder_t<Actor, Handler, details::count::_0, void> {
-    /** \brief alias for intrusive pointer for an actor type */
-    using typed_actor_ptr_t = intrusive_ptr_t<Actor>;
-
-    /** \brief alias for boost::asio supervior */
-    using typed_sup_t = supervisor_asio_t;
-
-    /** \brief constructs forwarder for the actor from it handler function */
-    forwarder_t(Actor &actor_, Handler &&handler_) : typed_actor{&actor_}, handler{std::move(handler_)} {}
+template <typename Actor, typename Handler>
+struct forwarder_t<Actor, Handler, details::count::_0, void> : forwarder_base_t<Actor, Handler, void> {
+    using base_t = forwarder_base_t<Actor, Handler, void>;
+    using base_t::base_t;
 
     /** \brief mimics boost::asio handler, which will be forwarded/decomposed into
      * two different methods of the actor
      */
     template <typename T = void> inline void operator()() noexcept {
-        auto &sup = static_cast<typed_sup_t &>(typed_actor->get_supervisor());
+        using typed_sup_t = typename base_t::typed_sup_t;
+
+        auto &sup = static_cast<typed_sup_t &>(base_t::typed_actor->get_supervisor());
         auto &strand = get_strand(sup);
-        asio::defer(strand, [actor = typed_actor, handler = std::move(handler)]() {
+        asio::defer(strand, [actor = base_t::typed_actor, handler = std::move(base_t::handler)]() {
             ((*actor).*handler)();
             actor->get_supervisor().do_process();
         });
     }
-
-    /** intrusive pointer to the actor */
-    typed_actor_ptr_t typed_actor;
-
-    /** pointer-to-member function of the actor to handle positive case result */
-    Handler handler;
 };
 
 /** \brief forwarder specialization, when error case is not possible  */
-template <typename Actor, typename Handler> struct forwarder_t<Actor, Handler, details::count::_1, void> {
-    /** \brief alias for intrusive pointer for an actor type */
-    using typed_actor_ptr_t = intrusive_ptr_t<Actor>;
-
-    /** \brief alias for boost::asio supervior */
-    using typed_sup_t = supervisor_asio_t;
-
-    /** \brief constructs forwarder for the actor from it handler function */
-    forwarder_t(Actor &actor_, Handler &&handler_) : typed_actor{&actor_}, handler{std::move(handler_)} {}
+template <typename Actor, typename Handler>
+struct forwarder_t<Actor, Handler, details::count::_1, void> : forwarder_base_t<Actor, Handler, void> {
+    using base_t = forwarder_base_t<Actor, Handler, void>;
+    using base_t::base_t;
 
     /** \brief mimics boost::asio handler, which will be forwarded to the appropriate
      * actor's member method.
      */
     template <typename T> inline void operator()(T arg) noexcept {
-        auto &sup = static_cast<typed_sup_t &>(typed_actor->get_supervisor());
+        using typed_sup_t = typename base_t::typed_sup_t;
+        auto &sup = static_cast<typed_sup_t &>(base_t::typed_actor->get_supervisor());
         auto &strand = get_strand(sup);
-        asio::defer(strand, [actor = typed_actor, handler = std::move(handler), arg = std::move(arg)]() mutable {
+        asio::defer(strand, [actor = base_t::ttyped_actor, handler = std::move(base_t::handler),
+                             arg = std::move(arg)]() mutable {
             ((*actor).*handler)(std::move(arg));
             actor->get_supervisor().do_process();
         });
     }
-
-    /** intrusive pointer to the actor */
-    typed_actor_ptr_t typed_actor;
-
-    /** pointer-to-member function of the actor to handle positive case result */
-    Handler handler;
 };
 
 /** construtor deduction guide for forwarder */
