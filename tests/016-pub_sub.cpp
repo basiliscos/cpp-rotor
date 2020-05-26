2208865198b863860269d6573a57e33ca52d5f6d
Ivan Baidakou <the.dmol@yandex.by>
2020-05-26 09:26:07 +0300

    Enable one more test

--- tests/016-pub_sub.cpp
+++ tests/016-pub_sub.cpp
@@ -13,14 +13,34 @@ namespace rt = r::test;
 
 struct payload_t {};
 
+struct pub_config_t: r::actor_config_t {
+    r::address_ptr_t pub_addr;
+    using r::actor_config_t::actor_config_t;
+};
+
+template <typename Actor> struct pub_config_builder_t: r::actor_config_builder_t<Actor> {
+    using builder_t = typename Actor::template config_builder_t<Actor>;
+    using parent_t = r::actor_config_builder_t<Actor>;
+    using parent_t::parent_t;
+
+
+    builder_t&& pub_addr(const r::address_ptr_t& addr) {
+        parent_t::config.pub_addr = addr;
+        return std::move(*static_cast<builder_t *>(this));
+    }
+
+    bool validate() noexcept override { return parent_t::config.pub_addr && parent_t::validate(); }
+};
+
+
 struct pub_t : public r::actor_base_t {
+    using config_t = pub_config_t;
+    template <typename Actor> using config_builder_t = pub_config_builder_t<Actor>;
 
-    using r::actor_base_t::actor_base_t;
+    explicit pub_t (config_t& cfg): r::actor_base_t(cfg), pub_addr{cfg.pub_addr} {}
 
-    void set_pub_addr(const r::address_ptr_t &addr) { pub_addr = addr; }
-
-    void on_start(r::message_t<r::payload::start_actor_t> &msg) noexcept override {
-        r::actor_base_t::on_start(msg);
+    void on_start() noexcept override {
+        r::actor_base_t::on_start();
         send<payload_t>(pub_addr);
     }
 
@@ -28,18 +48,21 @@ struct pub_t : public r::actor_base_t {
 };
 
 struct sub_t : public r::actor_base_t {
-    std::uint16_t received = 0;
-    using r::actor_base_t::actor_base_t;
+    using config_t = pub_config_t;
+    template <typename Actor> using config_builder_t = pub_config_builder_t<Actor>;
 
-    void set_pub_addr(const r::address_ptr_t &addr) { pub_addr = addr; }
+    explicit sub_t (config_t& cfg): r::actor_base_t(cfg), pub_addr{cfg.pub_addr} {}
 
-    void init_start() noexcept override {
-        subscribe(&sub_t::on_payload, pub_addr);
-        r::actor_base_t::init_start();
+    void configure(r::plugin_t& plugin) noexcept override {
+        if (plugin.identity() == r::internal::starter_plugin_t::class_identity) {
+            auto& p = static_cast<r::internal::starter_plugin_t&>(plugin);
+            p.subscribe_actor(&sub_t::on_payload, pub_addr);
+        }
     }
 
     void on_payload(r::message_t<payload_t> &) noexcept { ++received; }
 
+    std::uint16_t received = 0;
     r::address_ptr_t pub_addr;
 };
 
@@ -48,13 +71,9 @@ TEST_CASE("ping-pong", "[supervisor]") {
 
     auto sup = system_context.create_supervisor<rt::supervisor_test_t>().timeout(rt::default_timeout).finish();
     auto pub_addr = sup->create_address();
-    auto pub = sup->create_actor<pub_t>().timeout(rt::default_timeout).finish();
-    auto sub1 = sup->create_actor<sub_t>().timeout(rt::default_timeout).finish();
-    auto sub2 = sup->create_actor<sub_t>().timeout(rt::default_timeout).finish();
-
-    pub->set_pub_addr(pub_addr);
-    sub1->set_pub_addr(pub_addr);
-    sub2->set_pub_addr(pub_addr);
+    auto pub = sup->create_actor<pub_t>().pub_addr(pub_addr).timeout(rt::default_timeout).finish();
+    auto sub1 = sup->create_actor<sub_t>().pub_addr(pub_addr).timeout(rt::default_timeout).finish();
+    auto sub2 = sup->create_actor<sub_t>().pub_addr(pub_addr).timeout(rt::default_timeout).finish();
 
     sup->do_process();
 
