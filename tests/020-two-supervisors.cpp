4b9795d7c9ba3a9bcc1459a080a49140d175fe83
Ivan Baidakou <the.dmol@yandex.by>
2020-05-27 10:44:08 +0300

    Enable more test

--- tests/020-two-supervisors.cpp
+++ tests/020-two-supervisors.cpp
@@ -15,21 +15,33 @@ struct my_supervisor_t : public rt::supervisor_test_t {
     using rt::supervisor_test_t::supervisor_test_t;
 
     void init_start() noexcept override {
-        if (state == r::state_t::INITIALIZING) {
-            on_initialize_count++;
-        }
         rt::supervisor_test_t::init_start();
+        assert(state == r::state_t::INITIALIZING);
+        init_start_count++;
+    }
+
+    void init_finish() noexcept override {
+        rt::supervisor_test_t::init_finish();
+        assert(state == r::state_t::INITIALIZED);
+        init_finish_count++;
+    }
+
+    void shutdown_start() noexcept override {
+        rt::supervisor_test_t::shutdown_start();
+        shutdown_start_count++;
+        assert(state == r::state_t::SHUTTING_DOWN);
     }
 
     void shutdown_finish() noexcept override {
         rt::supervisor_test_t::shutdown_finish();
-        if (state == r::state_t::SHUTTED_DOWN) {
-            on_shutdown_count++;
-        }
+        shutdown_finish_count++;
+        assert(state == r::state_t::SHUTTED_DOWN);
     }
 
-    std::uint32_t on_initialize_count = 0;
-    std::uint32_t on_shutdown_count = 0;
+    std::uint32_t init_start_count = 0;
+    std::uint32_t init_finish_count = 0;
+    std::uint32_t shutdown_start_count = 0;
+    std::uint32_t shutdown_finish_count = 0;
 };
 
 TEST_CASE("two supervisors, different localities", "[supervisor]") {
@@ -47,49 +59,71 @@ TEST_CASE("two supervisors, different localities", "[supervisor]") {
     sup1->do_process();
     REQUIRE(sup1->get_state() == r::state_t::INITIALIZING);
     REQUIRE(sup2->get_state() == r::state_t::INITIALIZING);
-    REQUIRE(sup1->on_initialize_count == 1);
-    REQUIRE(sup2->on_initialize_count == 0);
+    REQUIRE(sup1->init_start_count == 1);
+    REQUIRE(sup1->init_finish_count == 0);
+    REQUIRE(sup2->init_start_count == 1);
+    REQUIRE(sup2->init_finish_count == 0);
 
     sup2->do_process();
     REQUIRE(sup1->get_state() == r::state_t::INITIALIZING);
     REQUIRE(sup2->get_state() == r::state_t::INITIALIZED);
-    REQUIRE(sup1->on_initialize_count == 1);
-    REQUIRE(sup2->on_initialize_count == 1);
-    REQUIRE(sup1->on_shutdown_count == 0);
+    REQUIRE(sup1->init_start_count == 1);
+    REQUIRE(sup1->init_finish_count == 0);
+    REQUIRE(sup2->init_start_count == 1);
+    REQUIRE(sup2->init_finish_count == 1);
 
     sup1->do_process();
-    REQUIRE(sup1->on_initialize_count == 1);
-    REQUIRE(sup2->on_initialize_count == 1);
+    REQUIRE(sup1->init_start_count == 1);
+    REQUIRE(sup1->init_finish_count == 1);
+    REQUIRE(sup2->init_start_count == 1);
+    REQUIRE(sup2->init_finish_count == 1);
     REQUIRE(sup1->get_state() == r::state_t::OPERATIONAL);
     REQUIRE(sup2->get_state() == r::state_t::INITIALIZED);
+    REQUIRE(sup1->shutdown_start_count == 0);
 
     sup2->do_process();
     REQUIRE(sup2->get_state() == r::state_t::OPERATIONAL);
-    REQUIRE(sup1->on_initialize_count == 1);
-    REQUIRE(sup2->on_initialize_count == 1);
-    REQUIRE(sup2->on_shutdown_count == 0);
-    REQUIRE(sup1->on_shutdown_count == 0);
+    REQUIRE(sup1->init_start_count == 1);
+    REQUIRE(sup1->init_finish_count == 1);
+    REQUIRE(sup2->init_start_count == 1);
+    REQUIRE(sup2->init_finish_count == 1);
+    REQUIRE(sup2->shutdown_start_count == 0);
+    REQUIRE(sup1->shutdown_start_count == 0);
 
     sup2->do_shutdown();
     sup2->do_process();
+    REQUIRE(sup1->shutdown_start_count == 0);
+    REQUIRE(sup1->shutdown_finish_count == 0);
+    REQUIRE(sup2->shutdown_start_count == 0);
+    REQUIRE(sup2->shutdown_finish_count == 0);
+
     sup1->do_process();
+    REQUIRE(sup1->shutdown_start_count == 0);
+    REQUIRE(sup1->shutdown_finish_count == 0);
+    REQUIRE(sup2->shutdown_start_count == 0);
+    REQUIRE(sup2->shutdown_finish_count == 0);
+
     sup2->do_process();
-
     REQUIRE(sup1->get_state() == r::state_t::OPERATIONAL);
     REQUIRE(sup2->get_state() == r::state_t::SHUTTED_DOWN);
-    REQUIRE(sup1->on_shutdown_count == 0);
-    REQUIRE(sup2->on_shutdown_count == 1);
+    REQUIRE(sup1->shutdown_start_count == 0);
+    REQUIRE(sup1->shutdown_finish_count == 0);
+    REQUIRE(sup2->shutdown_start_count == 1);
+    REQUIRE(sup2->shutdown_finish_count == 1);
 
     sup1->do_process();
     REQUIRE(sup1->get_state() == r::state_t::OPERATIONAL);
     REQUIRE(sup2->get_state() == r::state_t::SHUTTED_DOWN);
-    REQUIRE(sup1->on_shutdown_count == 0);
-    REQUIRE(sup2->on_shutdown_count == 1);
+    REQUIRE(sup1->shutdown_start_count == 0);
+    REQUIRE(sup1->shutdown_finish_count == 0);
+    REQUIRE(sup2->shutdown_start_count == 1);
+    REQUIRE(sup2->shutdown_finish_count == 1);
 
     sup1->do_shutdown();
     sup1->do_process();
     REQUIRE(sup1->get_state() == r::state_t::SHUTTED_DOWN);
-    REQUIRE(sup1->on_shutdown_count == 1);
+    REQUIRE(sup1->shutdown_start_count == 1);
+    REQUIRE(sup1->shutdown_finish_count == 1);
 
     REQUIRE(sup1->get_leader_queue().size() == 0);
     REQUIRE(sup1->get_points().size() == 0);
@@ -99,6 +133,7 @@ TEST_CASE("two supervisors, different localities", "[supervisor]") {
     REQUIRE(sup2->get_points().size() == 0);
     REQUIRE(sup2->get_subscription().size() == 0);
 }
+
 
 TEST_CASE("two supervisors, same locality", "[supervisor]") {
     r::system_context_t system_context;
@@ -114,14 +149,17 @@ TEST_CASE("two supervisors, same locality", "[supervisor]") {
     sup1->do_process();
     REQUIRE(sup1->get_state() == r::state_t::OPERATIONAL);
     REQUIRE(sup2->get_state() == r::state_t::OPERATIONAL);
-    REQUIRE(sup2->on_initialize_count == 1);
+    REQUIRE(sup1->init_start_count == 1);
+    REQUIRE(sup1->init_finish_count == 1);
+    REQUIRE(sup2->init_start_count == 1);
+    REQUIRE(sup2->init_finish_count == 1);
 
     sup1->do_shutdown();
     sup1->do_process();
 
     REQUIRE(sup1->get_state() == r::state_t::SHUTTED_DOWN);
     REQUIRE(sup2->get_state() == r::state_t::SHUTTED_DOWN);
-    REQUIRE(sup1->on_shutdown_count == 1);
+    REQUIRE(sup1->shutdown_start_count == 1);
 
     REQUIRE(sup1->get_leader_queue().size() == 0);
     REQUIRE(sup1->get_points().size() == 0);
@@ -146,7 +184,6 @@ TEST_CASE("two supervisors, down internal first, same locality", "[supervisor]")
     sup1->do_process();
     REQUIRE(sup1->get_state() == r::state_t::OPERATIONAL);
     REQUIRE(sup2->get_state() == r::state_t::OPERATIONAL);
-    REQUIRE(sup2->on_initialize_count == 1);
 
     // for better coverage
     sup2->template send<r::payload::shutdown_trigger_t>(sup2->get_address(), sup2->get_address());
