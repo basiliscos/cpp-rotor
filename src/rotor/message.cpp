12acb91c88ab39ab621bab13f615248c855dfd9f
Ivan Baidakou <the.dmol@yandex.by>
2022-04-10 12:54:25 +0300

    minor optimization

--- src/rotor/message.cpp
+++ src/rotor/message.cpp
@@ -1,13 +1,13 @@
 #include "rotor/message.h"
 
-using type_map_t = std::unordered_map<std::string, const void *>;
+using type_map_t = std::unordered_map<std::string_view, const void *>;
 
 namespace rotor::message_support {
 
 const void *register_type(const std::type_index &type_index) noexcept {
     static type_map_t type_map = {};
 
-    auto name = std::string(type_index.name());
+    auto name = std::string_view(type_index.name());
     auto it = type_map.find(name);
     if (it != type_map.end()) {
         return it->second;
